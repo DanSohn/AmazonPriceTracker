79b7e408b68d1d8a245af05eda213183addf565d
Daniel <chiefgustavo000@gmail.com>
2019-10-11 15:16:10 -0600

    Used request and soup to grab the page's item name and current price. Instead of a single soup, I used two where the second grabbed the prettify'd version of the first as that helped solve the issue where I wouldn't be able to get the text of title and price, instead returning an error message NoneType has no attribute 'get_text'

--- AmazonPriceTracker.py
+++ AmazonPriceTracker.py
@@ -10,3 +10,26 @@ In email, sending the url as a link, not just as a string
 """
 
 import requests
+from bs4 import BeautifulSoup
+
+URL = "https://www.amazon.ca/Mattress-Underblanket-Controller-Settings-Auto-Off/dp/B07G85QKK7/ref=sr_1_7?keywords=electric+blanket&qid=1570827364&sr=8-7"
+
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36"}
+
+# returns all the data from the URL provided
+page = requests.get(URL, headers = headers)
+
+# issues along the line if I use just one soup. I use two so that the soup find method will not return NoneType
+soup1 = BeautifulSoup(page.content, "html.parser")
+soup2 = BeautifulSoup(soup1.prettify(), "html.parser")
+
+# from amazon, grab the span productTitle which corresponds to the title of the item
+# .get_text = get rid of span tags
+# .strip() = return only non-whitespace string
+title = soup2.find(id = "productTitle").get_text().strip()
+#title = title.get_text()
+#title = title.strip()
+
+price = soup2.find(id = "priceblock_saleprice").get_text().strip()
+print(price, "\n")
+print(title)
