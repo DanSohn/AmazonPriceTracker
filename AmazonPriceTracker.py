39a89eb3d6240b018483e488ba2f079589df4db3
Daniel <chiefgustavo000@gmail.com>
2019-10-11 14:53:44 -0600

    Added a paragraph of what I want to do in this project

---
+++ AmazonPriceTracker.py
@@ -0,0 +1,12 @@
+"""
+A quick project where I can get a url from an amazon page, and then given credentials and a price range,
+it will email the user when the item falls under the price range
+Things to do:
+Get information from amazon page
+Get user's information
+Send email
+Do I want it running every hour? Do I need to write a script to have it turned on when computer turns on
+In email, sending the url as a link, not just as a string
+"""
+
+import requests
